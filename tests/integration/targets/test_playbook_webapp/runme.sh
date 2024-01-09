b45004cdd555a12f507b59172a3e6d18d7439399
abikouo <abikouo@redhat.com>
2024-01-09 12:34:17 +0100

    Remove unused playbook

--- tests/integration/targets/test_playbook_webapp/runme.sh
+++ tests/integration/targets/test_playbook_webapp/runme.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-# generate inventory with access_key provided through a templated variable
-ansible-playbook create_aws_credentials.yml "$@"
-source access_key.sh
-
 set -eux
 
 function cleanup() {
-    set +x
-    source access_key.sh
-    set -x
-    ansible-playbook webapp.yaml -e "operation=delete" "$@"
+    ansible-playbook test_webapp.yaml -e "operation=delete" "$@"
     exit 1
 }
 
 trap 'cleanup "${@}"'  ERR
 
 # Create web application
-ansible-playbook webapp.yaml "$@"
+ansible-playbook test_webapp.yaml "$@"
 
 # Delete web application
-ansible-playbook webapp.yaml -e "operation=delete" "$@"
+ansible-playbook test_webapp.yaml -e "operation=delete" "$@"
